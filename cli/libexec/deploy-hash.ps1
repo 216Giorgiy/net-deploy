960b6299610231b0fdb19b3f04f99bde82586b5b
Luke Sampson <me@lukesampson.com>
2014-05-14 10:41:58 +1000

    deploy hash

--- cli/libexec/deploy-hash.ps1
+++ cli/libexec/deploy-hash.ps1
@@ -1,14 +1,16 @@
-# Usage: deploy hash <username> <password>
-# Summary: Get a hashed username/password entry
-# Help: This command is used to generate a hashed username/password entry to be used in the users configuration file on the deploy web server.
+# Usage: deploy hash <username>
+# Summary: Get a hashed user configuration entry
+# Help: This command is used to generate an entry to be used in the users configuration file on the deploy web server.
 #
-# It outputs a line in the format username iterations:salt:hash
-param($username, $password)
+# It will prompt you to enter a password for the given username.
+#
+# It outputs a line in the format "username iterations:salt:hash"
+param($username)
 
+. "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\help.ps1"
 
 if(!$username) { my_usage; exit 1 }
-if(!$password) { my_usage; exit 1 }
 
 function base64($bytes) {
 	[convert]::tobase64string($bytes)
@@ -25,6 +27,26 @@ function pbkdf2($password, $salt, $iterations, $bytesize) {
 	$pbkdf2 = new-object security.cryptography.rfc2898derivebytes $password, $salt
 	$pbkdf2.iterationcount = $iterations
 	$pbkdf2.getbytes($bytesize)
+}
+
+# convert secure string back to string
+# from http://blogs.msdn.com/b/fpintos/archive/2009/06/12/how-to-properly-convert-securestring-to-string.aspx
+function unsecure($secure) {
+	$ptr = [intptr]::zero
+	$marshal = [runtime.interopservices.marshal]
+	try {
+		$ptr = $marshal::SecureStringToGlobalAllocUnicode($secure)
+		return $marshal::PtrToStringUni($ptr)
+	} finally {
+		$marshal::ZeroFreeGlobalAllocUnicode($ptr)
+	}
+}
+
+$password = unsecure (read-host "password for $username" -assecurestring)
+$confirm = unsecure (read-host "re-enter password" -assecurestring)
+
+if($confirm -ne $password) {
+	abort "password didn't match!"
 }
 
 $ITERATIONS = 1000
