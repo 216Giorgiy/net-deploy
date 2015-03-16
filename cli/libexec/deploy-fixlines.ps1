a3bb0a98144bf7f9f098ac17800aaf9d7a436293
Luke Sampson <me@lukesampson.com>
2015-03-16 20:58:39 +1000

    faster fixlines

--- cli/libexec/deploy-fixlines.ps1
+++ cli/libexec/deploy-fixlines.ps1
@@ -11,10 +11,14 @@ $changes = git status --porcelain
 
 $changes | % {
     $type, $path = $_.substring(0,2), "$root\$($_.substring(3))"
-    if($type -ne ' D' -and (gc $path -delimiter "`0" | sls "[^`r]`n")) {
-        write-host "Fixing line endings for $path..." -nonewline
-        $content = gc $_
-        $content | sc $_
-        write-host 'done.'
+
+    if($type -ne ' D') {
+        $content = [io.file]::readalltext($path)
+        if($content | sls "[^`r]`n") {
+            write-host "Fixing line endings for $path..." -nonewline
+            $content = gc $path
+            $content | sc $path
+            write-host 'done.'
+        }
     }
 }
