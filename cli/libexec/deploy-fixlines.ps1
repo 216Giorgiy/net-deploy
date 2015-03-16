6c1d9924cfa29db2d858d6cb346d86b4dc8d48c2
Luke Sampson <me@lukesampson.com>
2015-03-16 20:09:20 +1000

    fixlines shouldn't check files deleted from git repo

--- cli/libexec/deploy-fixlines.ps1
+++ cli/libexec/deploy-fixlines.ps1
@@ -7,11 +7,12 @@
 # http://stackoverflow.com/questions/724083/unix-newlines-to-windows-newlines-on-windows
 
 $root = root
-$changes = git status --porcelain |% { "$root\$($_.substring(3))" }
+$changes = git status --porcelain
 
 $changes | % {
-    if(gc $_ -delimiter "`0" | sls "[^`r]`n") {
-        write-host "Fixing line endings for $_..." -nonewline
+    $type, $path = $_.substring(0,2), "$root\$($_.substring(3))"
+    if($type -ne ' D' -and (gc $path -delimiter "`0" | sls "[^`r]`n")) {
+        write-host "Fixing line endings for $path..." -nonewline
         $content = gc $_
         $content | sc $_
         write-host 'done.'
