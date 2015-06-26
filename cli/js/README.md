5ca482828b4e0ed451d5b328032f91b350e0a444
Luke Sampson <me@lukesampson.com>
2015-06-26 18:07:40 +1000

    deploy hash working

---
+++ cli/js/README.md
@@ -0,0 +1,15 @@
+# Deploy
+### Command Line Interface
+
+A command line interface for [net-deploy](https://github.com/lukesampson/net-deploy).
+
+Usage examples:
+
+```
+deploy help # show help
+deploy help [cmd] # show help for a command
+
+deploy init # initialize a project to deploy
+deploy # deploys the project
+deploy hash [username] # creates a users.txt entry
+```
--- cli/js/libexec/deploy-hash.js
+++ cli/js/libexec/deploy-hash.js
@@ -1,9 +1,25 @@
-var core = require('../lib/core.js')
+var core = require('../lib/core.js');
+var pbkdf2 = require('pbkdf2');
+var crypto = require('crypto');
+
+
+const ITERATIONS = 1000;
+const SALT_SIZE = 24;
+const HASH_SIZE = 24;
 
 exports.exec = function(username) {
   if(!username) {
     core.abort('username is required');
   }
 
-  console.log("hash: " + username);
+  core.getpass('password: ', function(err, password) {
+    if(err) { process.exit(1); }
+    
+    console.log("oh, so your password is " + password);
+    var salt = crypto.randomBytes(SALT_SIZE);
+    var hash = pbkdf2.pbkdf2Sync(password, salt, ITERATIONS, HASH_SIZE, 'sha1');
+
+    console.log(username + ' ' + ITERATIONS + ':' +
+      salt.toString('base64') + ':' + hash.toString('base64'));
+  });
 }
